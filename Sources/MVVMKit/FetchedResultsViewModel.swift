3b62310b81d71fc5ec93e0ab027de61970285bdc
Jérôme Danthinne <jerome@grincheux.be>
2020-08-15 17:20:02 +0200

    FetchedResultsViewModel: only send didChange notification

--- Sources/MVVMKit/FetchedResultsViewModel.swift
+++ Sources/MVVMKit/FetchedResultsViewModel.swift
@@ -9,18 +9,6 @@
 import CoreData
 import UIKit
 
-public enum FetchedResultsChange {
-    case insert(indexPath: IndexPath)
-    case update(indexPath: IndexPath)
-    case move(sourceIndexPath: IndexPath, destinationIndexPath: IndexPath)
-    case delete(indexPath: IndexPath)
-}
-
-public enum FetchedResultsSectionChange {
-    case insert(indexSet: IndexSet)
-    case delete(indexPath: IndexSet)
-}
-
 public protocol FetchedResultsViewModelDelegate: AnyObject {
     func fetchedResultsViewModel(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
                                  shouldCallObserverFor type: AnyClass) -> Bool
@@ -31,15 +19,8 @@ open class FetchedResultsViewModel<Model: NSManagedObject>: NSObject, NSFetchedR
     private let fetchedResultsController: NSFetchedResultsController<Model>
     public weak var delegate: FetchedResultsViewModelDelegate?
 
-    public typealias Observer = (_ object: Model, _ change: FetchedResultsChange) -> Void
-    var observer: Observer?
-
-    public typealias SectionsObserver = (_ change: FetchedResultsSectionChange) -> Void
-    var sectionsObserver: SectionsObserver?
-
     public typealias ChangeObserver = () -> Void
-    var willChangeObserver: ChangeObserver?
-    var didChangeObserver: ChangeObserver?
+    var observer: ChangeObserver?
 
     public init(viewContext: NSManagedObjectContext,
                 fetchRequest: NSFetchRequest<Model>,
@@ -57,27 +38,12 @@ open class FetchedResultsViewModel<Model: NSManagedObject>: NSObject, NSFetchedR
 
     // MARK: - Observers
 
-    public func observe(_ observer: @escaping Observer) {
+    public func observe(_ observer: @escaping ChangeObserver) {
         self.observer = observer
-    }
-
-    public func sectionsObserve(_ observer: @escaping SectionsObserver) {
-        sectionsObserver = observer
-    }
-
-    public func willChangeObserve(_ observer: @escaping ChangeObserver) {
-        willChangeObserver = observer
-    }
-
-    public func didChangeObserve(_ observer: @escaping ChangeObserver) {
-        didChangeObserver = observer
     }
 
     public func cancelObservers() {
         observer = nil
-        sectionsObserver = nil
-        willChangeObserver = nil
-        didChangeObserver = nil
     }
 
     // MARK: - Getters
@@ -129,50 +95,9 @@ open class FetchedResultsViewModel<Model: NSManagedObject>: NSObject, NSFetchedR
 
     // MARK: - NSFetchedResultsControllerDelegate
 
-    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        willChangeObserver?()
-    }
-
     public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        didChangeObserver?()
-    }
-
-    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        if let sectionsObserver = sectionsObserver {
-            switch type {
-            case .insert:
-                sectionsObserver(.insert(indexSet: IndexSet(integer: sectionIndex)))
-            case .delete:
-                sectionsObserver(.delete(indexPath: IndexSet(integer: sectionIndex)))
-            default:
-                break
-            }
-        }
-    }
-
-    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        if delegate?.fetchedResultsViewModel(controller, shouldCallObserverFor: Model.self) ?? true,
-            let observer = observer,
-            let object = anObject as? Model {
-            let change: FetchedResultsChange
-            switch type {
-            case .insert:
-                change = .insert(indexPath: newIndexPath!)
-            case .delete:
-                change = .delete(indexPath: indexPath!)
-            case .move:
-                if indexPath == newIndexPath {
-                    change = .update(indexPath: indexPath!)
-                } else {
-                    change = .move(sourceIndexPath: indexPath!, destinationIndexPath: newIndexPath!)
-                }
-            case .update:
-                change = .update(indexPath: indexPath!)
-            @unknown default:
-                fatalError("Unhandled case")
-            }
-
-            observer(object, change)
+        if delegate?.fetchedResultsViewModel(controller, shouldCallObserverFor: Model.self) ?? true {
+            observer?()
         }
     }
 }
